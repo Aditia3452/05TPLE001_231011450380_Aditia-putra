4890b199eb2507048d884229509fd4b919d2748f
Aditia3452 <Aditiaputraa24@gmail.com>
2026-07-05 13:43:17 +0700

    if ELse

---
+++ Mahasiswa/Tugas/ifelse.py
@@ -0,0 +1,12 @@
+nilai = int(input("Masukkan nilai (0-100): "))
+
+if nilai >= 90:
+    print("Grade: A")
+elif nilai >= 80:
+    print("Grade: B")
+elif nilai >= 70:
+    print("Grade: C")
+elif nilai >= 60:
+    print("Grade: D")
+else:
+    print("Grade: E")
